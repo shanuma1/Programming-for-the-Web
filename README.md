01c0cbea3a5591021c689f8bd89568c9516357db
Sushma Hanumathu <shanuma1@binghamton.edu>
2020-06-24 08:17:26 +0530

    Project Started
    Added initial files according to the hints.

--- README.md
+++
@@ -1,1 +0,0 @@
-Summer 2020 CS 444/544 Programming for the Web Repository
--- slides/async/code/later.js
+++
@@ -1,7 +0,0 @@
-const MAX_TIMEOUT = 3;
-
-function later(fn, ...args) {
-  const timeout =
-    Math.floor(Math.random()*(MAX_TIMEOUT + 1));
-  setTimeout(fn, timeout*1000, ...args);
-}
